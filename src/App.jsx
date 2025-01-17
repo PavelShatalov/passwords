f61267308255507d9d0ec7464c95410ecb919e55
Pavel Shatalov <p.sh.cpp@mail.ru>
2025-01-17 02:17:50 +0100

    2

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,113 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+	const [password, setPassword] = useState("");
+	const [passwordLength, setPasswordLength] = useState(12);
+	const [useSymbols, setUseSymbols] = useState(true);
+	const [useNumbers, setUseNumbers] = useState(true);
+	const [useLowerCase, setUseLowerCase] = useState(true);
+	const [useUpperCase, setUseUpperCase] = useState(true);
+	const [successMessage, setSuccessMessage] = useState("");
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+	function generatePassword() {
+		let newPassword = "";
+		let charset = "";
+		if (useSymbols) charset += "!@#$%^&*()";
+		if (useNumbers) charset += "0123456789";
+		if (useLowerCase) charset += "abcdefghijklmnopqrstuvwxyz";
+		if (useUpperCase) charset += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		for (let i = 0; i < passwordLength; i++) {
+			newPassword += charset.charAt(Math.floor(Math.random() * charset.length));
+		}
+		setPassword(newPassword);
+	}
 
-export default App
+	function copyPassword() {
+		navigator.clipboard
+			.writeText(password)
+			.then(() => {
+				setSuccessMessage("password is copied");
+			})
+			.catch((error) => {
+				console.error(`password don't copied ${error}`);
+				setSuccessMessage("");
+			});
+	}
+
+	return (
+		<div>
+			<form>
+				<input type="text" value={password} readOnly />
+				<input type="button" value="copy" onClick={copyPassword} />
+			</form>
+
+			<form>
+				<label htmlFor="Lenght"> </label>
+				<input
+					type="number"
+					name="Lenght"
+					id=""
+					value={passwordLength}
+					onChange={(e) => {
+						setPasswordLength(Number(e.target.value));
+						// console.log(passwordLength);
+					}}
+				/>
+				<label htmlFor="Uppercase">Uppercase</label>
+				<input
+					type="checkbox"
+					checked
+					name="Uppercase"
+					id=""
+					onChange={() => {
+						setUseUpperCase(!useUpperCase);
+						// console.log(passwordLength);
+					}}
+					value={useUpperCase}
+				/>
+				<label htmlFor="Lowercase">Lowercase</label>
+				<input
+					type="checkbox"
+					checked
+					name="Lowercase"
+					id=""
+					onChange={() => {
+						setUseLowerCase(!useLowerCase);
+						// console.log(passwordLength);
+					}}
+					value={useLowerCase}
+				/>
+				<label htmlFor="Numbers">Numbers</label>
+				<input
+					type="checkbox"
+					checked
+					name="Numbers"
+					id=""
+					onChange={() => {
+						setUseNumbers(!useNumbers);
+					}}
+					value={useNumbers}
+				/>
+				<label htmlFor="Symbols">Symbols</label>
+				<input
+					type="checkbox"
+					checked
+					name="Symbols"
+					id=""
+					onChange={() => {
+						setUseSymbols(!useSymbols);
+					}}
+					value={useSymbols}
+				/>
+				<input type="button" value="generate" onClick={generatePassword} />
+			</form>
+			{successMessage && (
+				<>
+					<p>{successMessage}</p>
+				</>
+			)}
+		</div>
+	);
+};
+
+export default App;
